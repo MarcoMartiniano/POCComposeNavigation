648c20bec189c545634e6a23a284dcb8ed656db5
MarcoMartiniano <marrco.programacao@gmail.com>
2024-02-26 18:52:36 -0300

    Navigate using NavigationManager.kt

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # POCComposeNavigation
-Multi-module navigation with compose and bottomNavigation
+Multi-module navigation with compose and bottomNavigationBar, using Koin as DI.
+
+This proof of concept(POC) was made to evaluate navigation using a bottomNavigation bar with its own navController and
+navigation outside bottomNavigationBar(e.g. if you need a login screen before or don't want to show the bottomNavigationBar)
+
+Take as example HomeScreen to use NavigationManager with a separate navigation layer.
+Take as example HomeScreen2 or HomeScreen3 if you want to navigate passing onClick action as parameter. 
+(you can also see an epic branch created to show this approach).
--- core/navigation/src/main/java/com/example/navigation/destination/core/NavigationManager.kt
+++ core/navigation/src/main/java/com/example/navigation/destination/core/NavigationManager.kt
@@ -1,7 +1,5 @@
 package com.example.navigation.destination.core
 
-import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavOptions
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.navOptions
@@ -61,24 +59,6 @@ class NavigationManager(
     fun popStackBack() {
         applicationScope.launch {
             _commands.send(NavigationCommand.PopStackBack)
-        }
-    }
-
-    fun switchTabs(route: String, navController: NavController) {
-        navController.navigate(route) {
-            // Pop up to the start destination of the graph to
-            // avoid building up a large stack of destinations
-            // on the back stack as users select items
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
-            }
-
-            // Avoid multiple copies of the same destination when
-            // reselecting the same item
-            launchSingleTop = true
-
-            // Restore state when reselecting a previously selected item
-            restoreState = true
         }
     }
 }
